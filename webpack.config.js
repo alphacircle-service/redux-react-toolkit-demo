103d9043e548064161ad46d9f2d0df07a2de270d
JUNE <choie0423@naver.com>
2022-06-25 19:00:21 +0900

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -42,11 +42,9 @@ module.exports = {
 
     devServer : {
         historyApiFallback: true,
-        // inline: true,
         port : 3000,
         hot: true,
         compress:true,
-        // publicPath : '/',
         static: {
             directory: path.join(__dirname, 'public')
         }
